a665f022980d89420eb6d70e81671eff36893966
YT <35086553+Jcyt-R@users.noreply.github.com>
2020-03-20 16:08:24 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -213,7 +213,7 @@ const project = "https://github.com/CareyWang/sub-web";
 const remoteConfigSample =
   "https://raw.githubusercontent.com/tindy2013/subconverter/master/base/config/example_external_config.ini";
 const gayhubRelease = "https://github.com/tindy2013/subconverter/releases";
-const defaultBackend = "https://api.ytoo-163cdn.com/sub?";
+const defaultBackend = "https://api.wcc.best/sub?";
 const shortUrlBackend = "https://suo.yt/short";
 const configUploadBackend = "https://api.wcc.best/config/upload";
 const tgBotLink = "https://t.me/CareyWong_bot";
